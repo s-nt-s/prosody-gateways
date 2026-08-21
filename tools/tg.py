8ec1492723376cb7a7ec6ddd2787d87f30b77b80
s-nt-s <santos82h@gmail.com>
2026-08-21 16:04:38 +0200

    herramienta para obtener ids de telegram

--- tools/tg.py
+++ tools/tg.py
@@ -14,11 +14,65 @@ from telethon.tl.custom.dialog import Dialog
 from telethon.tl.patched import Message
 from telethon.tl.types.contacts import ResolvedPeer
 from telethon.errors.rpcerrorlist import PhoneNotOccupiedError
+import logging
 
 from os import chdir
 from os.path import dirname, abspath, isfile
 
 chdir(dirname(abspath(__file__)))
+
+logger = logging.getLogger(__name__)
+
+
+class MyClient(TelegramClient):
+    async def get_user(self, phone: str):
+        try:
+            r = await self(
+                functions.contacts.ResolvePhoneRequest(
+                    phone=phone
+                )
+            )
+        except PhoneNotOccupiedError:
+            return None
+        if not isinstance(r, ResolvedPeer):
+            return None
+        users: list[User] = []
+        for u in r.users:
+            if isinstance(u, User):
+                users.append(u)
+        size = len(users)
+        if size == 0:
+            return None
+        if size > 1:
+            logger.warning(f"{phone} has {size} users")
+        return users[0]
+
+
+    async def iter_users(self):
+        async for dialog in self.iter_dialogs():
+            if not isinstance(dialog, Dialog):
+                continue
+            entity = dialog.entity
+
+            if isinstance(entity, User):
+                yield entity
+                continue
+
+            try:
+                async for user in self.iter_participants(entity):
+                    yield user
+                continue
+            except:
+                pass
+
+            async for message in self.iter_messages(
+                entity,
+                limit=MESSAGES_PER_DIALOG,
+            ):
+                if isinstance(entity, Message):
+                    if isinstance(message.sender, User):
+                        yield message.sender
+
 
 def read_json(path: str) -> dict[str, int]:
     if not isfile(path):
@@ -27,35 +81,49 @@ def read_json(path: str) -> dict[str, int]:
         return json.load(f)
 
 def write_json(path: str, data: dict):
-    data = dict(sorted(data.items()))
+    data = dict(sorted(data.items(), key=lambda x: (x[1] is None, x)))
     with open(path, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=2, ensure_ascii=False)
 
 
 OUTPUT_FILE = "tg.json"
 BATCH_SIZE = 100
-MESSAGES_PER_DIALOG = 10000
+MESSAGES_PER_DIALOG = 100000
 REQUEST_DELAY = 3.1
 
 API_ID = int(os.environ["TG_API_ID"])
 API_HASH = os.environ["TG_API_HASH"]
 DATA: dict[str, int] = read_json(OUTPUT_FILE)
 
-PHONES = tuple(sorted(set(sys.argv[1:]).difference(DATA.keys())))
+
+def parse_num(t: str):
+    if len(t) == 9 and not t.startswith("+"):
+        return "+34"+t
+    if t[0]!='+':
+        raise ValueError(t)
+    return t
+
+
+PHONES = tuple(sorted(
+    set(map(parse_num, sys.argv[1:])).difference(DATA.keys())
+))
 
 
 def add_kv(p:str, i: int):
-    p = f"+{p}"
-    print(p, i)
+    p = parse_num(p)
+    if i is not None and DATA.get(p) != i:
+        print(p, i)
     DATA[p] = i
+    if len(DATA) % 100 == 0:
+        write_json(OUTPUT_FILE, DATA)
 
 
 def add_user(user):
     if isinstance(user, User) and user.phone:
-        add_kv(user.phone, user.id)
+        add_kv('+'+user.phone, user.id)
 
 
-async def do_lookup(client: TelegramClient, phones: tuple[str]):
+async def do_lookup(client: MyClient, phones: tuple[str]):
     results: dict[str, int] = {}
     contacts = [
         InputPhoneContact(
@@ -74,53 +142,30 @@ async def do_lookup(client: TelegramClient, phones: tuple[str]):
     )
 
     for user in response.users:
-        if user.phone:
-            ph = f"+{user.phone}"
-            results[ph] = user.id
+        add_user(user)
 
     return results
 
 
-async def do_import(client: TelegramClient, phones: tuple[str]):
+async def do_import(client: MyClient, phones: tuple[str]):
     for offset in range(0, phones, BATCH_SIZE):
-        batch = phones[offset:offset + BATCH_SIZE]
+        if offset > 0:
+            await asyncio.sleep(1)
         DATA.update(
-            await do_lookup(client, batch)
+            await do_lookup(
+                client,
+                phones[offset:offset + BATCH_SIZE]
+            )
         )
 
-        # Evita encadenar inmediatamente todas las peticiones.
-        if offset + BATCH_SIZE < len(phones):
-            await asyncio.sleep(1)
 
-
-async def do_search(client: TelegramClient):
-    async for dialog in client.iter_dialogs():
-        if not isinstance(dialog, Dialog):
-            continue
-        entity = dialog.entity
-
-        if isinstance(entity, User) and entity.phone:
-            add_user(entity)
-            continue
-
-        try:
-            async for user in client.iter_participants(entity):
-                add_user(user)
-            continue
-        except:
-            pass
-
-        async for message in client.iter_messages(
-            entity,
-            limit=MESSAGES_PER_DIALOG,
-        ):
-            if not isinstance(entity, Message):
-                continue
-            add_user(message.sender)
+async def do_search(client: MyClient):
+    async for user in client.iter_users():
+        add_user(user)
 
 
 async def do_main():
-    client = TelegramClient(
+    client = MyClient(
         "telegram_lookup",
         API_ID,
         API_HASH,
@@ -128,7 +173,7 @@ async def do_main():
     await client.start()
 
     try:
-        await do_search(client)
+        #await do_search(client)
         await resolve_lookup(
             client,
             tuple(sorted(set(PHONES).difference(DATA.keys())))
@@ -143,23 +188,22 @@ async def do_main():
     write_json(OUTPUT_FILE, DATA)
 
 
-async def resolve_lookup(client: TelegramClient, phones: tuple[str, ...]):
+async def resolve_lookup(client: MyClient, phones: tuple[str, ...]):
     for p in phones:
         await asyncio.sleep(REQUEST_DELAY)
-        try:
-            response = await client(
-                functions.contacts.ResolvePhoneRequest(
-                    phone=p
-                )
-            )
-        except PhoneNotOccupiedError:
-            continue
-        if not isinstance(response, ResolvedPeer):
-            continue
-
-        for user in response.users:
-            add_user(user)
+        user = await client.get_user(p)
+        if user is None:
+            add_kv(p, None)
+        else:
+            add_kv(p, user.id)
 
 
 if __name__ == "__main__":
+    if False:
+        PHONES = set()
+        for k, v in list(DATA.items()):
+            if v is None:
+                del DATA[k]
+                PHONES.add(k)
+        PHONES = tuple(sorted(PHONES))
     asyncio.run(do_main())
