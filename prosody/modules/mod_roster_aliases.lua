25d4d67744b530d1670c202421639fe8a1cb0f0b
s-nt-s <santos82h@gmail.com>
2026-08-28 00:33:55 +0200

    Squashed commit of the following:
    commit c57f5aa390b889cd0dbd8a16ca439eb38d08c0c6
    Author: s-nt-s <santos82h@gmail.com>
    Date:   Fri Aug 28 00:30:41 2026 +0200
    
        mod
    
    commit 7c9b3abc611d525dba59a583e704897cdeb21c96
    Author: s-nt-s <santos82h@gmail.com>
    Date:   Wed Aug 26 16:57:46 2026 +0200
    
        proteger bookmark
    
    commit 68af31d92a40f4c5f0a1614f99de36fca4f07e41
    Author: s-nt-s <santos82h@gmail.com>
    Date:   Wed Aug 26 16:39:52 2026 +0200
    
        soporte para bookmarks
    
    commit 18a88f676429d5dad7e4259853ad3ea5ef152cc5
    Author: s-nt-s <santos82h@gmail.com>
    Date:   Wed Aug 26 15:50:49 2026 +0200
    
        roster
    
    commit 7b1e0f1f4aa50f28652b22b3add4729baf6247fa
    Author: s-nt-s <santos82h@gmail.com>
    Date:   Wed Aug 26 01:04:45 2026 +0200
    
        avatar
    
    commit 9761f459a60c8a5ff8006c20566e625f3fe53c0b
    Author: s-nt-s <santos82h@gmail.com>
    Date:   Wed Aug 26 00:44:14 2026 +0200
    
        avatares
    
    commit bc464e1f8a8d12acfaeba796263d8ccfbbb3198c
    Author: s-nt-s <santos82h@gmail.com>
    Date:   Tue Aug 25 23:51:23 2026 +0200
    
        usar rostermanager

--- prosody/modules/mod_roster_aliases.lua
+++ prosody/modules/mod_roster_aliases.lua
@@ -7,11 +7,9 @@ local aliases_file = module:get_option_string(
     "/var/lib/prosody/roster-names.json"
 )
 
-local roster_defaults = setmetatable({}, { __mode = "k" })
-local aliases_signatures = setmetatable({}, { __mode = "k" })
-
 module:log("info", "roster_aliases loaded on host %s", module.host)
 
+-- Lee los aliases en cada operación para aceptar cambios sin reiniciar Prosody.
 local function load_aliases()
     local file = io.open(aliases_file, "r")
     if not file then
@@ -28,8 +26,28 @@ local function load_aliases()
     return {}, content
 end
 
-local function apply_alias(aliases, jid, item)
-    local alias = aliases[jid or item.jid]
+-- Compara mapas de grupos sin depender del orden en que fueron definidos.
+local function groups_equal(left, right)
+    if type(left) ~= "table" or type(right) ~= "table" then
+        return left == right
+    end
+    for group, enabled in pairs(left) do
+        if right[group] ~= enabled then
+            return false
+        end
+    end
+    for group, enabled in pairs(right) do
+        if left[group] ~= enabled then
+            return false
+        end
+    end
+    return true
+end
+
+-- Aplica al contacto el nombre y los grupos definidos en el archivo JSON.
+local function apply_alias(aliases, contact_jid, item)
+    local normalized_jid = jid.prep(contact_jid or item.jid)
+    local alias = normalized_jid and aliases[normalized_jid]
     if type(alias) ~= "table" then
         return
     end
@@ -47,46 +65,58 @@ local function apply_alias(aliases, jid, item)
                 groups[group] = true
             end
         end
-        item.groups = groups
-    end
-end
-
-local function save_roster_defaults(roster)
-    local defaults = {}
-    for jid, item in pairs(roster) do
-        if type(item) == "table" and type(jid) == "string" then
-            defaults[jid] = {
-                name = item.name,
-                groups = item.groups,
-            }
+        if not groups_equal(item.groups, groups) then
+            item.groups = groups
         end
     end
-    roster_defaults[roster] = defaults
 end
 
+-- Aplica los aliases al roster y devuelve los contactos que realmente cambiaron.
 local function apply_roster(roster, aliases)
     if type(roster) ~= "table" then
-        return
+        return false, {}
     end
-    local defaults = roster_defaults[roster]
-    if not defaults then
-        save_roster_defaults(roster)
-        defaults = roster_defaults[roster]
-    end
+    local changed = false
+    local changed_jids = {}
     for jid, item in pairs(roster) do
         if type(item) == "table" and type(jid) == "string" then
-            local default = defaults[jid]
-            if default then
-                item.name = default.name
-                item.groups = default.groups
-            end
+            local old_name = item.name
+            local old_groups = item.groups
             apply_alias(aliases, jid, item)
+            if old_name ~= item.name or old_groups ~= item.groups then
+                changed = true
+                changed_jids[#changed_jids + 1] = jid
+            end
         end
     end
+    return changed, changed_jids
 end
 
+-- Guarda los cambios usando la API oficial y notifica a las sesiones conectadas.
+local function persist_roster_aliases(username, host, roster, aliases)
+    local changed, changed_jids = apply_roster(roster, aliases)
+    if not changed then
+        return true
+    end
+    if not roster_manager.save_roster(username, host, roster) then
+        module:log(
+            "error",
+            "Could not persist aliases for roster %s@%s",
+            username,
+            host
+        )
+        return false
+    end
+    for _, contact_jid in ipairs(changed_jids) do
+        roster_manager.roster_push(username, host, contact_jid)
+    end
+    return true
+end
+
+-- Modifica un item de una petición IQ antes de que Prosody lo persista.
 local function apply_stanza_alias(item, aliases)
-    local alias = aliases[item.attr.jid]
+    local item_jid = jid.prep(item.attr.jid)
+    local alias = item_jid and aliases[item_jid]
     if type(alias) ~= "table" then
         return
     end
@@ -110,116 +140,26 @@ local function apply_stanza_alias(item, aliases)
     end
 end
 
-local function apply_participant_alias(event)
-    local stanza = event.stanza
-    local origin_type = event.origin and event.origin.type or "nil"
-    local muc_user = stanza:get_child(
-        "x", "http://jabber.org/protocol/muc#user"
-    )
-    local item = muc_user and muc_user:get_child("item")
-    local bare_jid = item and item.attr.jid
-    local user_nick = stanza:get_child(
-        "nick", "http://jabber.org/protocol/nick"
-    )
-    module:log(
-        "debug",
-        "presence hook host=%s origin=%s type=%s from=%s to=%s item.jid=%s item.nick=%s nick=%s stanza=%s",
-        module.host,
-        origin_type,
-        stanza.attr.type or "available",
-        stanza.attr.from or "nil",
-        stanza.attr.to or "nil",
-        bare_jid or "nil",
-        item and item.attr.nick or "nil",
-        user_nick and user_nick[1] or "nil",
-        stanza
-    )
-
-    if origin_type ~= "component" then
-        return
-    end
-
-    local aliases = load_aliases()
-    local alias
-
-    if bare_jid then
-        alias = aliases[bare_jid] or aliases[jid.bare(bare_jid)]
-    end
-
-    if not alias and not bare_jid and stanza.attr.from then
-        local _, room_host, participant_resource = jid.split(stanza.attr.from)
-        if room_host and participant_resource then
-            bare_jid = participant_resource .. "@" .. room_host
-            alias = aliases[bare_jid]
-        end
-    end
-
-    if not alias and item then
-        local item_node, item_host = item.attr.jid and jid.split(item.attr.jid)
-        if item_node and item_host then
-            for alias_jid, candidate in pairs(aliases) do
-                local alias_node, alias_host = jid.split(alias_jid)
-                if alias_node == item_node and alias_host == item_host then
-                    alias = candidate
-                    break
-                end
-            end
-        end
-    end
-
-    if not alias then
-        module:log("debug", "No participant alias for item JID %s", bare_jid or "nil")
-        return
-    end
-
-    if type(alias) == "table"
-        and alias.name ~= json.null
-        and type(alias.name) == "string" then
-        if item then
-            item.attr.nick = alias.name
-        end
-        local from_room = stanza.attr.from and stanza.attr.from:match("^([^/]+)")
-        if from_room then
-            stanza.attr.from = from_room .. "/" .. alias.name
-        end
-        if user_nick then
-            user_nick[1] = alias.name
-        else
-            stanza:tag("nick", { xmlns = "http://jabber.org/protocol/nick" })
-                :text(alias.name)
-                :up()
-        end
-        module:log(
-            "info",
-            "Participant alias applied: %s -> %s; from=%s; item.nick=%s",
-            bare_jid,
-            alias.name,
-            stanza.attr.from or "nil",
-            item and item.attr.nick or "nil"
-        )
-    end
-end
-
 module:hook("roster-load", function(event)
-    save_roster_defaults(event.roster)
     local aliases = load_aliases()
-    apply_roster(event.roster, aliases)
+    persist_roster_aliases(event.username, event.host, event.roster, aliases)
 end)
 
+-- Reaplica la política cuando el cliente solicita su propio roster.
 module:hook("iq/self/jabber:iq:roster:query", function(event)
     if event.stanza.attr.type ~= "get" then
         return
     end
-    local aliases, signature = load_aliases()
-    local roster = event.origin.roster
-    if roster and roster[false]
-        and signature ~= aliases_signatures[roster] then
-        roster[false].version = (roster[false].version or 0) + 1
-        aliases_signatures[roster] = signature
-    end
-    apply_roster(roster, aliases)
+    local aliases = load_aliases()
+    persist_roster_aliases(
+        event.origin.username,
+        event.origin.host,
+        event.origin.roster,
+        aliases
+    )
 end, 1)
 
+-- Reaplica la política cuando un componente solicita el roster de un usuario.
 module:hook("iq-get/bare/jabber:iq:roster:query", function(event)
     local target = event.stanza.attr.to
     if not target then
@@ -232,15 +172,11 @@ module:hook("iq-get/bare/jabber:iq:roster:query", function(event)
     end
 
     local roster = roster_manager.load_roster(username, host)
-    local aliases, signature = load_aliases()
-    if roster and roster[false]
-        and signature ~= aliases_signatures[roster] then
-        roster[false].version = (roster[false].version or 0) + 1
-        aliases_signatures[roster] = signature
-    end
-    apply_roster(roster, aliases)
+    local aliases = load_aliases()
+    persist_roster_aliases(username, host, roster, aliases)
 end, 1)
 
+-- Protege los nombres y grupos frente a cambios enviados por gateways.
 module:hook("iq-set/bare/jabber:iq:roster:query", function(event)
     local target = event.stanza.attr.to
     if not target then
@@ -259,16 +195,3 @@ module:hook("iq-set/bare/jabber:iq:roster:query", function(event)
     end
 end, 1)
 
-module:hook("roster-item-added", function(event)
-    local aliases = load_aliases()
-    apply_alias(aliases, event.jid, event.item)
-end)
-
-module:hook("roster-item-updated", function(event)
-    local aliases = load_aliases()
-    apply_alias(aliases, event.jid, event.item)
-end)
-
-module:hook("presence/full", apply_participant_alias, 100)
-module:hook("presence/bare", apply_participant_alias, 100)
-module:hook("presence/host", apply_participant_alias, 100)
--- prosody/prosody.cfg.template.lua
+++ prosody/prosody.cfg.template.lua
@@ -5,6 +5,9 @@ plugin_paths = {
   "/usr/local/share/lua/5.1",
   "/etc/prosody/custom_modules"
 }
+log = {
+  { levels = { "info" }, to = "console" };
+}
 
 allow_registration = false
 c2s_require_encryption = true
@@ -58,7 +61,9 @@ Component "upload.{{XMPP_DOMAIN}}" "http_file_share"
   -- allow slidgram to use the upload component
   -- point generated upload URLs to the public host that actually serves the file share endpoint
   http_host = "{{XMPP_DOMAIN}}"
-  http_external_url = "https://{{XMPP_DOMAIN}}"
+  http_file_share_base_url = "https://{{XMPP_DOMAIN}}"
+  http_file_share_size_limit = 50 * 1024 * 1024
+  http_file_share_daily_quota = 500 * 1024 * 1024
   http_file_share_access = _http_file_share_access
 
 VirtualHost "{{XMPP_DOMAIN}}"
@@ -67,7 +72,8 @@ VirtualHost "{{XMPP_DOMAIN}}"
     certificate = "/etc/prosody/certs/{{XMPP_DOMAIN}}.crt";
   };
   modules_enabled = {
-    "privilege", "pep", "carbons", "offline", "mam", "roster_aliases"
+    "privilege", "pep", "bookmarks", "carbons", "offline", "mam",
+    "roster_aliases",
   }
   archive_expires_after = 0 -- guardar historial indefinidamente
   default_archive_policy = "roster"
@@ -82,25 +88,37 @@ VirtualHost "{{XMPP_DOMAIN}}"
 Component "{{TELEGRAM_COMPONENT_JID}}"
   component_secret = "{{SLIDGE_COMPONENT_SECRET}}"
   modules_enabled = {"privilege"}
-  http_file_share_expires_after = 86400   -- 1 día
+  privileged_entities = {
+    ["{{TELEGRAM_COMPONENT_JID}}"] = _privileges;
+  }
+  http_file_share_expires_after = 86400*3   -- 1 día
   http_file_share_access = _http_file_share_access
 
 Component "{{WHATSAPP_COMPONENT_JID}}"
   component_secret = "{{SLIDGE_COMPONENT_SECRET}}"
   modules_enabled = {"privilege"}
-  http_file_share_expires_after = 86400   -- 1 día
+  privileged_entities = {
+    ["{{WHATSAPP_COMPONENT_JID}}"] = _privileges;
+  }
+  http_file_share_expires_after = 86400*3 -- 1 día
   http_file_share_access = _http_file_share_access
 
 Component "{{STEAM_COMPONENT_JID}}"
   component_secret = "{{SLIDGE_COMPONENT_SECRET}}"
   modules_enabled = {"privilege"}
-  http_file_share_expires_after = 86400   -- 1 día
+  privileged_entities = {
+    ["{{STEAM_COMPONENT_JID}}"] = _privileges;
+  }
+  http_file_share_expires_after = 86400*3   -- 1 día
   http_file_share_access = _http_file_share_access
 
 Component "{{GOOGLE_COMPONENT_JID}}"
   component_secret = "{{SLIDGE_COMPONENT_SECRET}}"
   modules_enabled = {"privilege"}
-  http_file_share_expires_after = 86400   -- 1 día
+  privileged_entities = {
+    ["{{GOOGLE_COMPONENT_JID}}"] = _privileges;
+  }
+  http_file_share_expires_after = 86400*3   -- 1 día
   http_file_share_access = _http_file_share_access
 
 admins = { "{{XMPP_ADMIN}}" }
